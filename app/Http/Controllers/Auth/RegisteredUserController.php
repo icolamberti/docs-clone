e50955ab38c47c205a3d9e01e21fca7f6b73270b
Ico Lamberti <icolamberti@gmail.com>
2025-01-25 19:39:28 -0300

    Refactor: initial adjustments

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -10,24 +10,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
-use Inertia\Inertia;
 use Inertia\Response;
 
 class RegisteredUserController extends Controller
 {
-  /**
-   * Display the registration view.
-   */
   public function create(): Response
   {
-    return Inertia::render('Auth/Register');
+    return inertia('Auth/Register');
   }
 
-  /**
-   * Handle an incoming registration request.
-   *
-   * @throws \Illuminate\Validation\ValidationException
-   */
   public function store(Request $request): RedirectResponse
   {
     $request->validate([
--- routes/auth.php
+++ routes/auth.php
@@ -1,11 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
@@ -59,20 +57,6 @@ Route::middleware('auth')->group(function () {
   ])
     ->middleware('throttle:6,1')
     ->name('verification.send');
-
-  Route::get('confirm-password', [
-    ConfirmablePasswordController::class,
-    'show',
-  ])->name('password.confirm');
-
-  Route::post('confirm-password', [
-    ConfirmablePasswordController::class,
-    'store',
-  ]);
-
-  Route::put('password', [PasswordController::class, 'update'])->name(
-    'password.update'
-  );
 
   Route::post('logout', [
     AuthenticatedSessionController::class,
