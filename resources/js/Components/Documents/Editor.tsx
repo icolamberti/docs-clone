726ac02e7e94610af257018ab7bb4c97bcb939b3
Ico Lamberti <icolamberti@gmail.com>
2025-02-01 17:18:05 -0300

    feat: add underline extension and update toolbar for text formatting

--- resources/js/Components/Documents/Editor.tsx
+++ resources/js/Components/Documents/Editor.tsx
@@ -6,6 +6,7 @@ import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
+import Underline from '@tiptap/extension-underline'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import ImageResize from 'tiptap-extension-resize-image'
@@ -57,6 +58,7 @@ export default function () {
       TableRow,
       TableHeader,
       TableCell,
+      Underline,
     ],
     content: `
       <table>
--- resources/js/Components/Documents/Toolbar.tsx
+++ resources/js/Components/Documents/Toolbar.tsx
@@ -1,6 +1,19 @@
 import { cn } from '@/Lib/utils'
 import { useEditorStore } from '@/Stores/useEditorStore'
-import { LucideIcon, Undo2Icon } from 'lucide-react'
+import {
+  BoldIcon,
+  ItalicIcon,
+  ListTodoIcon,
+  LucideIcon,
+  MessageSquarePlusIcon,
+  PrinterIcon,
+  Redo2Icon,
+  RemoveFormattingIcon,
+  SpellCheckIcon,
+  UnderlineIcon,
+  Undo2Icon,
+} from 'lucide-react'
+import { Separator } from '../Ui/separator'
 
 interface ToolbarButtonProps {
   onClick?: () => void
@@ -41,12 +54,120 @@ export default function () {
         icon: Undo2Icon,
         onClick: () => editor?.chain().focus().undo().run(),
       },
+      {
+        label: 'Redo',
+        icon: Redo2Icon,
+        onClick: () => editor?.chain().focus().redo().run(),
+      },
+      {
+        label: 'Print',
+        icon: PrinterIcon,
+        onClick: () => window.print(),
+      },
+      {
+        label: 'Spell check',
+        icon: SpellCheckIcon,
+        onClick: () => {
+          const current = editor?.view.dom.getAttribute('spellcheck')
+          editor?.view.dom.setAttribute(
+            'spellcheck',
+            current === 'false' ? 'true' : 'false',
+          )
+        },
+      },
+    ],
+    [
+      {
+        label: 'Bold',
+        icon: BoldIcon,
+        isActive: editor?.isActive('bold'),
+        onClick: () => editor?.chain().focus().toggleBold().run(),
+      },
+      {
+        label: 'Italic',
+        icon: ItalicIcon,
+        isActive: editor?.isActive('italic'),
+        onClick: () => editor?.chain().focus().toggleItalic().run(),
+      },
+      {
+        label: 'Underline',
+        icon: UnderlineIcon,
+        isActive: editor?.isActive('underline'),
+        onClick: () => editor?.chain().focus().toggleUnderline().run(),
+      },
+    ],
+    [
+      {
+        label: 'Comment',
+        icon: MessageSquarePlusIcon,
+        onClick: () => console.log('TODO: comment'),
+        isActive: false, // TODO: enable when comment is active
+      },
+      {
+        label: 'List Todo',
+        icon: ListTodoIcon,
+        onClick: () => editor?.chain().focus().toggleTaskList().run(),
+        isActive: editor?.isActive('taskList'),
+      },
+      {
+        label: 'Remove Formatting',
+        icon: RemoveFormattingIcon,
+        onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+      },
     ],
   ]
 
   return (
     <div className='flex min-h-10 items-center gap-x-0.5 overflow-x-auto rounded-3xl bg-[#f1f4f9] px-2.5 py-0.5'>
       {sections[0].map(item => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {/* TODO: Font family */}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {/* TODO: Heading */}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {/* TODO: Font size */}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {sections[1].map(item => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {/* TODO: Text color */}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {/* TODO: Highlight color */}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {/* TODO: Link */}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {/* TODO: Align */}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {/* TODO: Line height */}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {/* TODO: List */}
+
+      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
+
+      {sections[2].map(item => (
         <ToolbarButton key={item.label} {...item} />
       ))}
     </div>
