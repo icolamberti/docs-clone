a78ef3e10cc2ed865d6070705c140c554b561ae9
Ico Lamberti <icolamberti@gmail.com>
2025-02-01 22:15:40 -0300

    feat: add text alignment extension and toolbar buttons for alignment options

--- resources/js/Components/Documents/Toolbar.tsx
+++ resources/js/Components/Documents/Toolbar.tsx
@@ -2,12 +2,18 @@ import { cn } from '@/Lib/utils'
 import { useEditorStore } from '@/Stores/useEditorStore'
 import { type Level } from '@tiptap/extension-heading'
 import {
+  AlignCenterIcon,
+  AlignJustifyIcon,
+  AlignLeftIcon,
+  AlignRightIcon,
   BoldIcon,
   ChevronDownIcon,
   HighlighterIcon,
   ImageIcon,
   ItalicIcon,
   Link2Icon,
+  ListIcon,
+  ListOrderedIcon,
   ListTodoIcon,
   LucideIcon,
   MessageSquarePlusIcon,
@@ -392,6 +398,106 @@ const LinkButton = () => {
   )
 }
 
+const AlignButton = () => {
+  const { editor } = useEditorStore()
+
+  const alignments = [
+    {
+      label: 'Align Left',
+      value: 'left',
+      icon: AlignLeftIcon,
+    },
+    {
+      label: 'Align Center',
+      value: 'center',
+      icon: AlignCenterIcon,
+    },
+    {
+      label: 'Align Right',
+      value: 'right',
+      icon: AlignRightIcon,
+    },
+    {
+      label: 'Justify',
+      value: 'justify',
+      icon: AlignJustifyIcon,
+    },
+  ]
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className='flex h-7 min-w-7 shrink-0 flex-col items-center justify-center overflow-hidden rounded-sm px-1.5 text-sm hover:bg-neutral-200/80'>
+          <AlignLeftIcon className='size-4' />
+        </button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className='flex flex-col gap-y-1 p-1'>
+        {alignments.map(({ label, value, icon: Icon }) => (
+          <button
+            key={value}
+            onClick={() => editor?.chain().focus().setTextAlign(value).run()}
+            className={cn(
+              'flex items-center gap-x-2 rounded-sm px-2 py-1 hover:bg-neutral-200/80',
+              editor?.isActive({ textAlign: value }) && 'bg-neutral-200/80',
+            )}
+          >
+            <Icon className='size-4' />
+
+            <span className='text-sm'>{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+const ListButton = () => {
+  const { editor } = useEditorStore()
+
+  const lists = [
+    {
+      label: 'Bullet List',
+      icon: ListIcon,
+      isActive: () => editor?.isActive('bulletList'),
+      onClick: () => editor?.chain().focus().toggleBulletList().run(),
+    },
+    {
+      label: 'Ordered List',
+      icon: ListOrderedIcon,
+      isActive: () => editor?.isActive('orderedList'),
+      onClick: () => editor?.chain().focus().toggleOrderedList().run(),
+    },
+  ]
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className='flex h-7 min-w-7 shrink-0 flex-col items-center justify-center overflow-hidden rounded-sm px-1.5 text-sm hover:bg-neutral-200/80'>
+          <ListIcon className='size-4' />
+        </button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className='flex flex-col gap-y-1 p-1'>
+        {lists.map(({ label, icon: Icon, onClick, isActive }) => (
+          <button
+            key={label}
+            onClick={onClick}
+            className={cn(
+              'flex items-center gap-x-2 rounded-sm px-2 py-1 hover:bg-neutral-200/80',
+              isActive() && 'bg-neutral-200/80',
+            )}
+          >
+            <Icon className='size-4' />
+
+            <span className='text-sm'>{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
 export default function () {
   const { editor } = useEditorStore()
 
@@ -494,11 +600,7 @@ export default function () {
         <ToolbarButton key={item.label} {...item} />
       ))}
 
-      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
-
       <TextColorButton />
-
-      <Separator orientation='vertical' className='h-6 bg-neutral-300' />
 
       <HighlightColorButton />
 
@@ -508,11 +610,11 @@ export default function () {
 
       <ImageButton />
 
-      {/* TODO: Align */}
+      <AlignButton />
 
       {/* TODO: Line height */}
 
-      {/* TODO: List */}
+      <ListButton />
 
       {sections[2].map(item => (
         <ToolbarButton key={item.label} {...item} />
